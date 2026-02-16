9e2d46c7dcfe04abc01a9c4252e2fc5e51316000
Web-dev-experts <itworksonmymachinedev@gmail.com>
2026-02-16 07:06:32 +0100

    small fixes

--- model/Leagues/leagueModel.js
+++ model/Leagues/leagueModel.js
@@ -4,40 +4,6 @@ const leagueSchema = new Schema({
   name: {
     type: String,
     required: [true, 'The league must have a name!'],
-    enum: [
-      'Bronze',
-      'Silver',
-
-      // Gold tiers
-      'GoldI',
-      'GoldII',
-
-      // Cuprite tiers
-      'CupriteI',
-      'CupriteII',
-      'CupriteIII',
-
-      // Obsidian tiers
-      'ObsidianI',
-      'ObsidianII',
-      'ObsidianIII',
-
-      // Diamond tiers
-      'DiamondI',
-      'DiamondII',
-      'DiamondIII',
-
-      // Uranium tiers
-      'UraniumI',
-      'UraniumII',
-      'UraniumIII',
-
-      // Aetherium tiers
-      'AetheriumI',
-      'AetheriumII',
-      'AetheriumIII',
-      'AetheriumIV',
-    ],
   },
   descritpion: {
     type: String,
